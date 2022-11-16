b804612376ec662bed6c1e1ba1be91635638ed98
YuriSilv <yuridtna2002@hotmail.com>
2022-11-15 21:40:42 -0300

    clean fuc

--- system.py
+++ system.py
@@ -5,12 +5,20 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 class pre_process(ABC):
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
+    def __init__(self, dataframe:str):
+        self.dataframe = pd.read_csv(f'data/{dataframe}')
 
     @abstractmethod
-    def clean_df():
-        pass
+    def clean_df(self, year:int):
+        self.dataframe.drop_duplicates(inplace=True)
+        df_recorte = self.dataframe[['Auction Title ', 'Department ', 'Close Date ','Winning Bid ','CC Fee', 'Auction Fee Total','Pay Status ','Expenses']].copy()
+        df_recorte = df_recorte.convert_dtypes()
+        df_recorte['Close Date '] = pd.to_datetime(df_recorte['Close Date '])
+        df_recorte = df_recorte[df_recorte['Pay Status '] == 'Successful']
+        df_recorte.Expenses.fillna(0,inplace=True)
+        df_recorte['CC Fee'].fillna(0,inplace=True)
+        df_year = df_recorte[df_recorte['Close Date '].dt.year == year]
+        return df_year
 
 class graphic_analysis():
     pass
